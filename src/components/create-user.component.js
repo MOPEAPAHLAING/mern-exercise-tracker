e27574be7ec56fc9f9f7c0ec92b3c4926df5c5bd
Moe Pa Pa Hlaing <moepapahlaing21@gmail.com>
2020-11-13 20:00:16 +0630

    react create exercise

---
+++ src/components/create-user.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class CreateUser extends Component {
+    render() {
+        return(
+            <div>You are on the Create User component!</div>
+        )
+    }
+}
---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class EditExercise extends Component {
+    render() {
+        return(
+            <div>You are on the Edit Exercise component!</div>
+        )
+    }
+}
